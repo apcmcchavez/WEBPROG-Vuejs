ea74ec52d9c84c5b175b0382117a754f4f2fd1e3
Iyah <147396392+apcmcchavez@users.noreply.github.com>
2025-01-24 09:19:04 +0800

    add personal profile

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,110 @@
+<template>
+
+ <!-- Content Box-->
+  <div class="content-box">
+
+    <!--Main Heading-->
+    <header>
+      <h1 class="main-heading">Iyah Chavez' Personal Profile Web Page</h1>
+    </header>
+
+    <!-- About Me -->
+    <section id="about-me">
+      <h2 class="aboutme-heading"> ABOUT ME </h2>
+      <p>Hello I am Mariyah Vanna Monique C. Chavez. You can just call me "Iyah". I am the eldest of 3 siblings and am from the province of Kawit, Cavite</p>
+    </section>
+
+    <!-- Education Achievements -->
+    <section id="education">
+      <h3 class="section-heading"> EDUCATION ACHIEVEMENTS </h3>
+      <p>Some of the notable academic achievements I've accomplished would be:</p>
+      <ul>
+        <li>1st Year BSCS Student - Part of the Honors List for SoCIT in the Academic Year 2023 to 2024</li>
+        <li>Grade 12 - With Highest Honor & Best in Research Paper</li>
+        <li>Grade 11 - With High Honor</li>
+        <li>Grade 10 - With High Honor & Best in Research Paper</li>
+        <li>Grade 9 - With High Honor & Best in Research Paper</li>
+      </ul>
+      <p>Aside from these, I also have the opportunity to join English and Math related competitions during my elementary days to either represent my section in school or my school in our district.</p>
+    </section>
+
+    
+    <!-- Course -->
+    <section id="course">
+      <h3 class="section-heading"> COURSE </h3>
+      <p>Currently, I am under the course BSCS-SS, Bachelor of Science in Computer Science with specialization in Software Systems in Asia Pacific College.</p>
+    </section>
+
+    <!-- IT Experience -->
+    <section id="it-experience">
+      <h3 class="section-heading"> IT Experience </h3>
+      <p>Unfortunately, I do not have experience with IT prior to me becoming a student of APC under the Comp Sci course. The only experience I could say would be the things taught to me during my first year of college.</p>
+    </section>
+
+    <!-- Hobbies and Interests -->
+     <section id="hobbies-interests">
+      <h3 class="section-heading"> HOBBIES AND INTERESTS </h3>
+      <p> I would describe myself as someone without a definite hobby or interest; I enjoy experimenting. Below is a table of the various hobbies and interests I explore.</p>
+      <table>
+        <tr>
+          <th>Hobby/Interest</th>
+          <th>Details</th>
+        </tr>
+        <tr>
+          <td>Games</td>
+          <td>Valorant, Stardew Valley, Roblox, Honkai Star Rail, Zenless Zone Zero, Lethal Company, Cookie Run Kingdom</td>
+        </tr>
+        <tr>
+          <td>Arts and Crafts</td>
+          <td>Painting, Mosaic, Collage Making</td>
+        </tr>
+        <tr>
+          <td>Binge Watching English Series</td>
+          <td>Brooklyn Nine-nine, The Good Place, Big Bang Theory, The Modern Family, A Series of Unfortunate Events</td>
+        </tr>      
+      </table>
+     </section>
+
+     <!-- Goals in Life/ Dream -->
+    <section id="goals">
+      <h3 class="section-heading"> GOALS IN LIFE/DREAMS </h3>
+      <p>I want to be rich that I get to the point in life where I would not need to worry about indulging myself with my wants while at the same time I get to provide for my family.</p>
+    </section>
+
+    <!-- Picture Gallery -->
+    <section id="gallery">
+      <h3 class="section-heading"> PICTURE GALLERY</h3>
+      <p>Here are some pictures I want to share that gives you a glimpse of who I am as a person.</p>
+        <div class="gallery">
+          <div class="gallery-pic">Me and my Sister at Conquest 2023
+            <img
+            src="https://scontent.fmnl8-5.fna.fbcdn.net/v/t39.30808-6/347026332_939933630591911_7971404980830020827_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeF0n90DMIbif-FUwUNQAbNq7KlKJK5q9UrsqUokrmr1SjIj982ZQ7LrQ5TYV7ANzknNkz1qdN1R0_L5fp68bTrH&_nc_ohc=k0Gv1N5b4xsQ7kNvgEbRZ-t&_nc_zt=23&_nc_ht=scontent.fmnl8-5.fna&_nc_gid=AKH0oRamsoEZT9ue2EuJOJr&oh=00_AYCx0oH9PlNf-GWYPikwwU_GCTSfw39mF9MCbzyqbtAbcw&oe=674E60E9" 
+            alt="Me and my sister at Conquest 2023"
+            >
+          </div>
+          <div class="gallery-pic">SHS Graduation Pic
+            <img
+            src="https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.30808-6/348596668_1414657449268752_1890444115094119327_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGe29ePYd_r0hgaPIT_QMAmUKunyVLicKNQq6fJUuJwowwfBymR2fLwVl_25Ny4uNh0qlsIKIIpsLXEcDfrucrq&_nc_ohc=U_lSZ9n3HUYQ7kNvgF1Y_1r&_nc_zt=23&_nc_ht=scontent.fmnl8-3.fna&_nc_gid=ARaoY0LXSWSCj0_pI_e5_lF&oh=00_AYDTk7PLp_WMzSkafexduvuWw9iKeOPzhzd4Z0sqe2FD2Q&oe=674E63D2" 
+            alt="Me with my girl bestfriends during our SHS Graduation"
+            >
+          </div>
+          <div class="gallery-pic">Me Painting
+            <img
+            src="https://scontent.fmnl8-5.fna.fbcdn.net/v/t39.30808-6/315756626_6546064405420758_3169002179891738396_n.jpg?stp=dst-jpg_p526x296&_nc_cat=109&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGpDcXDJHhRakyRHLJsBVkUkn5a1l-xH9ySflrWX7Ef3NmqQjOM3u_Hl_KIfOx3VXSCOBEEjfD_qIC0bSUtigTS&_nc_ohc=MPTrtTPPBHYQ7kNvgHJaG55&_nc_zt=23&_nc_ht=scontent.fmnl8-5.fna&_nc_gid=AqySM1MIIpsuk12L12uqGfB&oh=00_AYCXUB4be_qHgGIAtWyynE4J7p0E4cIlYGmC1B4ITw4_2A&oe=674E5075" 
+            alt="Me doing a painting project"
+            >
+          </div>
+          <div class="gallery-pic">First Time Baking
+            <img
+            src="https://scontent.fmnl8-5.fna.fbcdn.net/v/t39.30808-6/467014272_9739089676118199_8102920527614594995_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeH9NBDNTl0ZPddENxCas-BoDAoybEHdDuMMCjJsQd0O46Ob6MXtuDqL_KL4iNkP1y_7s4I5LknIYtOCBEeQRRLl&_nc_ohc=oJzA-wIIjG4Q7kNvgFho18M&_nc_zt=23&_nc_ht=scontent.fmnl8-5.fna&_nc_gid=AKUy2nZ8YQeplP2qLe5_6yn&oh=00_AYAtteir0zd66-o8cphmjPmose-Ty45HEd589-_7r3cZcA&oe=674E30C5" 
+            alt="First time baking a cake"
+            >
+          </div>
+        </div>
+    </section>
+  </div>
+
+  </template>
+
+<script> </script>
+<style> </style>
