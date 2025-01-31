1d8c83732d2adaa310895e6519b36ca1e44c3dec
Iyah <147396392+apcmcchavez@users.noreply.github.com>
2025-01-31 01:19:04 +0000

    removed the 'testing' headings in template

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -16,15 +16,10 @@
 
 
 <template>
-
-  <h1> Testing </h1>
     <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
     </ul>
-
-
   <h1>Food</h1>
-  <h1> Testing </h1>
   <personal-profile/> <br>
   <food-item/> <br>
   <food-item2/> <br>
