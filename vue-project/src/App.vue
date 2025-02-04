818ecb6a959b9cd9d67d8b35c9a6b8259f7df4ed
Iyah <147396392+apcmcchavez@users.noreply.github.com>
2025-02-04 08:35:11 +0800

    repasted things

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,22 @@
-<script setup>
+  <script setup>
   import { ref, onMounted } from 'vue'
   import { supabase } from './lib/supabaseClient'
 
-  const countries = ref([])
+  const instruments = ref([])
 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
+  async function getInstruments() {
+    const { data } = await supabase.from('instruments').select()
+    instruments.value = data
   }
 
   onMounted(() => {
-    getCountries()
+    getInstruments()
   })
-</script>
-
+  </script>
 
 <template>
     <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
     </ul>
   <h1>Food</h1>
   <personal-profile/> <br>
